9543e6526b7e0aaaa820259cb6be156d631fa3d4
apermadi <acpermadi@gmail.com>
2023-12-28 18:48:52 -0800

    placeholder level and camera function

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -26,3 +26,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_reset"):
+		get_tree().reload_current_scene()
